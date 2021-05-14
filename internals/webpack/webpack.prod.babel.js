9600d88364697b02f347b5041994fbb13b83f110
jean-kesrewani <jean.kesrouani.intern@anghami.com>
2021-05-14 22:18:44 +0300

    updated project name

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -116,9 +116,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Drink with the bois',
+      short_name: 'Drink',
+      description: 'Demand any drink and you shall get it',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
